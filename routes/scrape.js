b12b6e5af4247e4cb468c224e77cc8a63eefe8f0
buckyinsfo <buckyinsfo@gmail.com>
2018-07-12 12:22:00 -0700

    Broken commit.  Params null.

--- routes/scrape.js
+++ routes/scrape.js
@@ -1,19 +1,15 @@
 const express       = require('express')
-const router        = express.Router()
+const router        = express.Router({mergeParams: true})
 const superagent    = require('superagent')
 const cheerio       = require('cheerio')
-
 const datetime      = require('node-datetime');
-var dt = datetime.create();
-var formatted = dt.format('m/d/Y H:M:S');
-
  
 /* GET users listing. */
 router.get('/', function(req, res, next) {
 
-    //const url = 'https://www.amazon.com/Raspberry-Pi-RASPBERRYPI3-MODB-1GB-Model-Motherboard/dp/B01CD5VC92/ref=sr_1_7'
-    const asin = 'B01CD5VC92'
+    const asin = req.params.asin ? req.params.asin : 'B01CD5VC92'//*/ 'B06XGPTPFH'
     const url = 'http://www.amazon.com/dp/'.concat( asin )
+    console.log( 'asin=' + req.params.asin )
     
     //?s=pc&ie=UTF8&qid=1531353552&sr=1-7&keywords=raspberry+pi+3+b%2B'
     const query = {
@@ -37,7 +33,6 @@ router.get('/', function(req, res, next) {
         const $ = cheerio.load(response.text)
         const timestamp = datetime.create().format('m/d/Y H:M:S');
 
-        //const body = $('body')
         const price = $('[id=price_inside_buybox]').html().trim()
         const title = $('[id=productTitle]').html().trim()
         const sellerBox = $('[id=shipsFromSoldByInsideBuyBox_feature_div]')
