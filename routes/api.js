f9eef5768aff33573b65e53afb6eff1475ab5b36
buckyinsfo <buckyinsfo@gmail.com>
2018-07-12 11:06:27 -0700

    Prune packages. Add dev/nodemon script locally. Add api route.

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+/* GET RESTful API listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a list of users');
+  res.send('respond with a api list');
 });
 
 module.exports = router;
